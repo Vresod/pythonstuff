2a4b1ec44db03d5665bee822e669ac87b0b52f73
Vresod <40863375+Vresod@users.noreply.github.com>
2018-10-09 16:52:27 -0400

    'f'

--- commit.py
+++ commit.py
@@ -1,7 +1,8 @@
 import os
 # -----------------------------------
 os.system('git add -A')
-comname = str(input('What is the commit name?: '))
+comname = str('\'' + input('What is the commit name?: ') + '\'')
 # -----------------------------------
 cmdrun = str('git commit -m' + comname)
 os.system(cmdrun)
+os.system('git push')
