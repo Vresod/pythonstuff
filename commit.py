bd444e653b5043a6c3043a9e3dbb127f9dd28286
ominousonix1337 <40872936+ominousonix1337@users.noreply.github.com>
2018-10-12 11:33:00 -0400

    FROG

--- commit.py
+++ commit.py
@@ -2,8 +2,8 @@ import os
 import time
 # -----------------------------------
 os.system('git add -A')
-comname = str(input('What is the commit name?: '))
 # -----------------------------------
+comname = str(input('What is the commit name?: '))
 cmdrun = str('git commit -m' + comname)
 os.system(cmdrun)
 os.system('git push')
